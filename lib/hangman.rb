cc6a9f3ed183e6b1bfdbb2e28c44a21a804d8910
Your Name <you@example.com>
2025-01-31 17:00:44 +0100

    add method to compare player guess and computer code and update board when requred

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,3 +1,5 @@
+puts "HIIII"
+
 module Includable
   AlPHABETS = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 end
@@ -7,13 +9,15 @@ class Board
  include Includable
   @@file_of_words = 'google-10000-english-no-swears.txt'
   
-  attr_accessor :secret_word
+  #puts AlPHABETS
+  attr_accessor :secret_word, :random_words, :board, :player_code
   def initialize
     @random_words = []
     @secret_word = []
     @count = 0
     @board = []
     @player = Player.new
+    @player_code = ""
   end
 
   def words_of_five_and_twelve
@@ -23,43 +27,52 @@ class Board
           @random_words << line.delete!("\n")
       end
     end
-    # @random_word
-    # @random_word.size
   end
 
   def select_rand_word
     @secret_word = @random_words.sample
-    p @secret_word = @secret_word.split("")
+    @secret_word = @secret_word.split("")
+    p @secret_word
+  end
+
+  def create_board
+    ln = @secret_word.length
+    @board = Array.new(ln, "_") 
   end
 
   def display_board
-    l = @secret_word.length
-    @board = Array.new(l, "_") 
-    puts @board
-  end
-
-  def update_board
-     
+    p @board
+    p AlPHABETS 
   end
   
   def check_codes?
-    @player.get_player_guess
-    if @secret_word.include?(@player_guess)
-      index = @secret_word.each_with_index.select{|c, i| c == @player_guess}.map(&:last)
+    @player_code = gets.chomp
+    if @secret_word.include?@player_code
+      p @player_code.class
+      index = @secret_word.each_with_index.select{|c, i| c == @player_code}.map(&:last)
       index.each do |i|
-        @board[i] = @player_guess
+        @board[i] = @player_code
+        update_alphabet_list()
       end
     else
-
+       puts "Incorrect Guess"
     end
+    display_board
+  end
+
+  def update_alphabet_list
+    AlPHABETS.map!{|x| x == @player_code ? "#" : x}
   end
 
 end
 
 class Player
+
+  attr_accessor :player_guess
   def initialize
+    puts "Let's have your name!"
     @player_name = gets.chomp
-    @player_guess = []
+    @player_guess = "a"
   end
   
   def get_player_guess
@@ -69,10 +82,11 @@ class Player
   
 end
 
-class Game
-  
-end
-
 board = Board.new
+
+
 board.words_of_five_and_twelve
 board.select_rand_word
+board.create_board
+board.display_board
+board.check_codes?
